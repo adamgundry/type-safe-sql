6dcbfb32bd7f17c82688e0d1601ab96904cbb55f
Adam Gundry <adam@well-typed.com>
2016-08-30 12:24:26 +0100

    Further hacking

--- TypeSafeSQL.hs
+++ TypeSafeSQL.hs
@@ -13,6 +13,7 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
+{-# OPTIONS_GHC -Wall -Wno-unticked-promoted-constructors -Wno-unused-imports -Wno-name-shadowing #-}
 
 module TypeSafeSQL where
 
@@ -29,25 +30,19 @@ type TableName  = Symbol
 type ColumnName = Symbol
 
 
--- SELECT [DISTINCT | ALL] result-column*
---   [FROM (table-or-subquery* | join-clause)] [WHERE expr] [GROUP BY expr* [HAVING expr]]
---   [ORDER BY ordering-term*] [LIMIT expr [OFFSET expr]]
-
--- result-column ::= * | expr [AS column-alias] | table-name . *
-
-data SelectQuery = Select { resultColumns :: [ResultColumn]
-                          , fromClause    :: TableExpr
-                          , whereClause   :: WhereClause
-                          }
+data SelectQuery = Select [ResultColumn] TableExpr WhereClause
 
 data TableExpr = From TableName
                | CrossJoin TableExpr TableExpr
+               | LeftJoinOn TableExpr TableExpr Expression
                | LeftJoinUsing TableExpr TableExpr [ColumnName]
+               | As TableExpr TableName
+
+data ResultColumn = Star | Expr Expression AsClause
 
 type WhereClause  = Maybe Expression
 type AsClause     = Maybe ColumnName
-data ResultColumn = Star | Expr Expression AsClause
-data Expression   = Lit | Col ColumnName
+data Expression   = IntLit | StrLit | Col ColumnName | Equal Expression Expression
 
 
 data family Sing k :: k -> Type
@@ -70,10 +65,14 @@ data instance Sing ResultColumn rc where
 data instance Sing TableExpr te where
   From_ :: Sing TableName t -> Sing TableExpr (From t)
   CrossJoin_ :: Sing TableExpr te1 -> Sing TableExpr te2 -> Sing TableExpr (CrossJoin te1 te2)
+  LeftJoinOn_ :: Sing TableExpr te1 -> Sing TableExpr te2 -> Sing Expression e -> Sing TableExpr (LeftJoinOn te1 te2 e)
   LeftJoinUsing_ :: Sing TableExpr te1 -> Sing TableExpr te2 -> Sing [ColumnName] cols -> Sing TableExpr (LeftJoinUsing te1 te2 cols)
+  As_ :: Sing TableExpr te -> Sing TableName t -> Sing TableExpr (As te t)
 data instance Sing Expression e where
-  Lit_ :: Int -> Sing Expression Lit
-  Col_ :: Sing ColumnName c -> Sing Expression (Col c)
+  IntLit_ :: Int -> Sing Expression IntLit
+  StrLit_ :: String -> Sing Expression StrLit
+  Col_    :: Sing ColumnName c -> Sing Expression (Col c)
+  Equal_  :: Sing Expression e1 -> Sing Expression e2 -> Sing Expression (Equal e1 e2)
 
 
 instance (s ~ x, KnownSymbol s) => IsLabel x (Sing Symbol s) where
@@ -102,15 +101,19 @@ instance ToQuery SelectQuery where
 instance ToQuery TableExpr where
   toQuery (From_ t) = toQuery t
   toQuery (CrossJoin_ te1 te2) = toQuery te1 ++ " CROSS JOIN " ++ toQuery te2
+  toQuery (LeftJoinOn_ te1 te2 e) = toQuery te1 ++ " LEFT OUTER JOIN" ++ toQuery te2 ++ " ON " ++ toQuery e
   toQuery (LeftJoinUsing_ te1 te2 cols) = toQuery te1 ++ " LEFT OUTER JOIN " ++ toQuery te2 ++ " USING " ++ toQuery cols
+  toQuery (As_ te t) = toQuery te ++ " AS " ++ toQuery t
 
 instance ToQuery ResultColumn where
   toQuery Star_ = "*"
   toQuery (Expr_ e mb_a) = toQuery e ++ maybeToQuery "AS" mb_a
 
 instance ToQuery Expression where
-  toQuery (Lit_ k) = show k
-  toQuery (Col_ c) = toQuery c
+  toQuery (IntLit_ k) = show k
+  toQuery (StrLit_ s) = show s
+  toQuery (Col_ c)    = toQuery c
+  toQuery (Equal_ e1 e2) = toQuery e1 ++ " == " ++ toQuery e2
 
 
 
@@ -134,9 +137,11 @@ type family QueryColumnsDB (db :: Database) (q :: SelectQuery) :: [Column] where
 type family ExpandTableExpr (db :: Database) (te :: TableExpr) :: [Column] where
   ExpandTableExpr db (From t) = LookupTable t db
   ExpandTableExpr db (CrossJoin te1 te2) = ExpandTableExpr db te1 ++ ExpandTableExpr db te2
+  ExpandTableExpr db (LeftJoinOn te1 te2 e)       = ExpandTableExpr db te1 ++ ExpandTableExpr db te2
   ExpandTableExpr db (LeftJoinUsing te1 te2 cols) = LookupColumns cols (ExpandTableExpr db te1)
                                                  ++ FilterColumns cols (ExpandTableExpr db te1)
                                                  ++ FilterColumns cols (ExpandTableExpr db te2)
+  ExpandTableExpr db (As te t) = ExpandTableExpr db te -- TODO
 
 type family LookupResultColumns (rcs :: [ResultColumn]) (cols :: [Column]) :: [Column] where
   LookupResultColumns '[] cols = '[]
@@ -145,12 +150,15 @@ type family LookupResultColumns (rcs :: [ResultColumn]) (cols :: [Column]) :: [C
   LookupResultColumns ('Expr e (Just c) ': rcs) cols = (c ::: ExprType e cols) ': LookupResultColumns rcs cols
 
 type family ExprName e where
-  ExprName Lit = "" -- ?
+  ExprName IntLit = "" -- ?
+  ExprName StrLit = "" -- ?
   ExprName (Col c) = c
 
 type family ExprType e cols where
-  ExprType Lit cols = Int -- ?
-  ExprType (Col c) cols = LookupColumn c cols
+  ExprType IntLit  _     = Int
+  ExprType StrLit  _     = String
+  ExprType (Col c) cols  = LookupColumn c cols
+  ExprType (Equal e1 e2) _ = Bool
 
 type family LookupTable (table :: TableName) (db :: Database) :: [Column] where
   LookupTable table '[] = TypeError (Text "Table " :<>: ShowType table :<>: Text " missing")
@@ -174,21 +182,38 @@ type family FilterColumns (colnames :: [ColumnName]) (cols :: [Column]) :: [Colu
 
 
 type family ValidQuery (db :: Database) (q :: SelectQuery) :: Constraint where
-  ValidQuery db (Select rcs t wc) = ValidWhere wc (QueryColumnsDB db (Select rcs t wc))
+  ValidQuery db (Select rcs t wc) = (ValidTableExpr db t, ValidWhere wc (QueryColumnsDB db (Select rcs t wc)))
+
+type family ValidTableExpr db t where
+  ValidTableExpr db (From t) = ValidTableName t db
+  ValidTableExpr db (CrossJoin te1 te2) = (ValidTableExpr db te1, ValidTableExpr db te2)
+  ValidTableExpr db (LeftJoinOn te1 te2 e) = (ValidTableExpr db te1, ValidTableExpr db te2, ExprType e (ExpandTableExpr db (LeftJoinOn te1 te2 e)) ~ Bool)
+  ValidTableExpr db (LeftJoinUsing te1 te2 cols) = (ValidTableExpr db te1, ValidTableExpr db te2, ValidCols cols (ExpandTableExpr db te1), ValidCols cols (ExpandTableExpr db te2))
+  ValidTableExpr db (As te t) = ValidTableExpr db te
 
 type family ValidWhere (wc :: WhereClause) (cols :: [Column])  :: Constraint where
   ValidWhere Nothing cols = ()
   ValidWhere (Just e) cols = ValidExpr e cols
 
 type family ValidExpr (e :: Expression) (cols :: [Column])  :: Constraint where
-  ValidExpr Lit cols = ()
+  ValidExpr IntLit  _ = ()
+  ValidExpr StrLit  _ = ()
   ValidExpr (Col c) cols = ValidCol c cols
+  ValidExpr (Equal e1 e2) cols = (ValidExpr e1 cols, ValidExpr e2 cols, ExprType e1 cols ~ ExprType e2 cols)
+
+type family ValidCols (colnames :: [ColumnName]) (cols :: [Column]) :: Constraint where
+  ValidCols '[] cols = ()
+  ValidCols (colname ': colnames) cols = (ValidCol colname cols, ValidCols colnames cols)
 
 type family ValidCol (c :: ColumnName) (cols :: [Column])  :: Constraint where
   ValidCol c '[] = TypeError (Text "Column " :<>: ShowType c :<>: Text " missing")
   ValidCol c ((c ::: _) ': _) = ()
   ValidCol c (_ ': cols) = ValidCol c cols
 
+type family ValidTableName (t :: TableName) (tables :: [Table])  :: Constraint where
+  ValidTableName t '[] = TypeError (Text "Table " :<>: ShowType t :<>: Text " missing")
+  ValidTableName t ((t := _) ': _) = ()
+  ValidTableName t (_ ': tables) = ValidTableName t tables
 
 
 type family All (c :: k -> Constraint) (xs :: [k]) :: Constraint where
@@ -219,16 +244,20 @@ select s = error (toQuery s)
 
 
 type ExampleQuery = Select '[ Expr (Col "name") {- AS -} (Just "foo")
-                            , Expr Lit Nothing
+                            , Expr IntLit Nothing
                             , Star
                             ]
                             (LeftJoinUsing (From "users") (From "posts") '["user_id"])
-                   {- WHERE -} (Just (Col "foo"))
+                   {- WHERE -} (Just (Col "foo" `Equal` StrLit))
 
-e :: Sing SelectQuery ExampleQuery
-e = Select_ (Expr_ (Col_ #name) (Just_ #foo) :> Expr_ (Lit_ 5) Nothing_ :> Star_ :> Nil_)
+e1 :: Sing SelectQuery ExampleQuery
+e1 = Select_ (Expr_ (Col_ #name) (Just_ #foo) :> Expr_ (IntLit_ 5) Nothing_ :> Star_ :> Nil_)
             (LeftJoinUsing_ (From_ #users) (From_ #posts) (#user_id :> Nil_))
-            (Just_ (Col_ #foo))
+            (Just_ (Col_ #foo `Equal_` StrLit_ "moo"))
+
+e2 = Select_ (Star_ :> Nil_)
+             (LeftJoinOn_ (From_ #posts) (From_ #users `As_` #blah) (Col_ #user_id `Equal_` Col_ #user_id))
+             Nothing_
 
 type MySchema = '[ "users" := '[ "user_id" ::: Int
                                , "name"    ::: String
@@ -241,4 +270,4 @@ type MySchema = '[ "users" := '[ "user_id" ::: Int
 
 type Example = QueryColumnsDB MySchema ExampleQuery
 
-blah = select @MySchema e
+blah = select @MySchema e1
